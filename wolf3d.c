c1ce73078f804f52892be412e849c1efc8ab4f5b
BoraY1923 <75539373+BoraY1923@users.noreply.github.com>
2026-02-21 15:43:23 +0300

    Add files via upload

---
+++ wolf3d.c
@@ -0,0 +1,170 @@
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define PI 3.1415926535
+#define P2 PI/2
+#define P3 3*PI/2
+
+float px,py,pdx,pdy,pa; //player position
+
+void drawPlayer()
+{
+	glColor3d(1,1,0);
+	glPointSize(8);
+	glBegin(GL_POINTS);
+	glVertex2i(px,py);
+	glEnd();
+	
+	glLineWidth(2);
+	glBegin(GL_LINES);
+	glVertex2i(px,py);
+	glVertex2i(px+pdx*5, py+pdy*5);
+	glEnd();
+}
+
+int mapX=8,mapY=8,mapS=64;
+int map[]=
+{
+	1,1,1,1,1,1,1,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,1,0,0,0,1,
+	1,0,0,1,0,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,0,1,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,1,1,1,1,1,1,1,
+};
+
+void drawMap2D()
+{
+	int x,y,xo,yo;
+	for(y=0;y<mapY;y++)
+	{
+		for(x=0;x<mapX;x++)
+		{
+			if(map[y*mapX+x]==1) {glColor3f(0,0,1);} else{glColor3f(1,1,1);}
+			xo=x*mapS;
+			yo=y*mapS;
+			glBegin(GL_QUADS);
+			glVertex2i(xo     +1, yo     +1);
+			glVertex2i(xo     +1, yo+mapS-1);
+			glVertex2i(xo+mapS-1, yo+mapS-1);
+			glVertex2i(xo+mapS-1, yo+1     );
+			glEnd();
+		}
+	}
+}
+
+float dist(float ax, float ay, float bx, float by, float ang)
+ {
+ 	return (sqrt((bx-ax)*(bx-ax)+(by-ay)*(by-ay)));
+ }
+
+void drawRays3D()
+{
+	int r,mx,my,mp,dof;
+	float rx,ry,ra,xo,yo;
+	ra=pa;
+	for (r=0;r<1;r++)
+	{
+		
+		//HORIZONTAL RAYS
+		float disH=1000000,hx=px,hy=py;
+		dof=0;
+		float aTan=-1/tan(ra);
+		if(ra>PI){ ry=(((int)py>>6)<<6)-0.0001; rx=(py-ry)*aTan+px; yo=-64; xo=-yo*aTan; } //looking up
+		if(ra<PI){ ry=(((int)py>>6)<<6)+64    ; rx=(py-ry)*aTan+px; yo= 64; xo=-yo*aTan; }
+		if (ra==0 || ra==PI) { rx=px; ry=py; dof=8;}
+		while(dof<8)
+		{
+			mx=(int)(rx)>>6;
+			my=(int)(ry)>>6;
+			mp=my*mapX+mx;
+			if(mp>0 && mp<mapX*mapY && map[mp]==1){ hx=rx; hy=ry; disH=dist(px,py,hx,hy,ra); dof=8;} //hit wall.
+			else{ rx+=xo; ry+= yo; dof+=1;}
+		}
+		/*
+		glColor3f(0,1,0);
+		glLineWidth(7);
+		glBegin(GL_LINES);
+		glVertex2i(px,py);
+		glVertex2i(rx,ry);
+		glEnd();
+		*/
+		
+		
+		//VERTICAL RAYS
+		float disV=1000000, vx=px,vy=py;
+		dof=0;
+		float nTan=-tan(ra);
+		if(ra>P2 && ra<P3){ rx=(((int)px>>6)<<6)-0.0001; ry=(px-rx)*nTan+py; xo=-64; yo=-xo*nTan; } //looking up
+		if(ra<P2 || ra>P3){ rx=(((int)px>>6)<<6)+64    ; ry=(px-rx)*nTan+py; xo= 64; yo=-xo*nTan; }
+		if (ra==0 || ra==PI) { rx=px; ry=py; dof=8;}
+		while(dof<8)
+		{
+			mx=(int)(rx)>>6;
+			my=(int)(ry)>>6;
+			mp=my*mapX+mx;
+			if(mp>0 && mp<mapX*mapY && map[mp]==1){ vx=rx; vy=ry; disV=dist(px,py,vx,vy,ra); dof=8;} //hit wall.
+			else{ rx+=xo; ry+= yo; dof+=1;}
+		}
+		if(disV<disH) {rx=vx; ry=vy;}
+		if (disH<disV){rx=hx; ry=hy;}
+		glColor3f(1,0,0);
+		glLineWidth(1);
+		glBegin(GL_LINES);
+		glVertex2i(px,py);
+		glVertex2i(rx,ry);
+		glEnd();
+	}	
+	
+	
+	
+}
+
+
+void display()
+{
+	glClear (GL_COLOR_BUFFER_BIT);
+	drawMap2D();
+	drawPlayer();
+	drawRays3D();
+	glutSwapBuffers();
+}
+
+
+/*==============
+keyboard buttons
+================*/
+void buttons(unsigned char key,int x,int y)
+{
+	if(key=='a') { pa-=0.1; if(pa<   0){ pa+=2*PI;} pdx=cos(pa) *5; pdy=sin(pa)*5;}
+	if(key=='d') { pa+=0.1; if(pa>2*PI){ pa-=2*PI;} pdx=cos(pa) *5; pdy=sin(pa)*5;}
+	if(key=='w') { px+=pdx; py+=pdy;}
+	if(key=='s') { px-=pdx; py-=pdy;}
+	glutPostRedisplay();
+} 
+
+
+void init()
+{
+	glClearColor(0.3,0.3,0.3,0);
+	gluOrtho2D(0,1024,512,0);
+	px=300;
+	py=300;
+	pdx=cos(pa)*5;
+	pdy=sin(pa)*5;
+	
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 512);
+	glutCreateWindow("Wolfenstein");
+	init();
+	glutDisplayFunc(display);
+	glutKeyboardFunc(buttons);
+	glutMainLoop();
+}
